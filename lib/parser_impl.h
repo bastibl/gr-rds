fc4f7e9f7adc88c1dcc0587007d110d49a35f8c1
Clayton Smith <argilo@gmail.com>
2024-11-01 17:26:19 -0400

    Only report complete program service names

--- lib/parser_impl.h
+++ lib/parser_impl.h
@@ -60,7 +60,8 @@ private:
 	unsigned char  pi_area_coverage;
 	unsigned char  pi_program_reference_number;
 	char           radiotext[65];
-	char           program_service_name[9];
+	char           program_service_name[8];
+	unsigned int   program_service_name_segment_flags;
 	bool           radiotext_AB_flag;
 	bool           traffic_program;
 	bool           traffic_announcement;
