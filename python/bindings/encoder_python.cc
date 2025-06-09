62944eb0a94235cbbd3f5517238b7cdc27e6f83f
Eric <compaq963@gmail.com>
2025-06-08 19:24:25 -0700

    Update encoder_python.cc
    Fix comment spacing

--- python/bindings/encoder_python.cc
+++ python/bindings/encoder_python.cc
@@ -13,7 +13,7 @@
 /* If manual edits are made, the following tags should be modified accordingly.    */
 /* BINDTOOL_GEN_AUTOMATIC(0)                                                       */
 /* BINDTOOL_USE_PYGCCXML(0)                                                        */
-/* BINDTOOL_HEADER_FILE(encoder.h)                                       	       */
+/* BINDTOOL_HEADER_FILE(encoder.h)                                       	     */
 /* BINDTOOL_HEADER_FILE_HASH(67c382b4126ed6cf3956c324eec3b60d)                     */
 /***********************************************************************************/
 
